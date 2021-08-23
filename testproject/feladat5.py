68d7f0e31819ade0e1bff081464b090c87b384ce
krfprogit <krfprogit@gmail.com>
2021-08-23 11:27:56 +0200

    feladat-5_megoldas-2: varosokat sorban ellenőrzi

--- testproject/feladat5.py
+++ testproject/feladat5.py
@@ -8,21 +8,25 @@ URL = "https://black-moss-0a0440e03.azurestaticapps.net/rv4.html"
 browser.get(URL)
 browser.maximize_window()
 
+# input elemek
 varos_input = browser.find_element_by_id("missingCity")
 ellenorzes_btn = browser.find_element_by_id("submit")
-varosok_text = browser.find_element_by_id("cites").text
-varosok_text_list = varosok_text.split(",")
+varosok_osszes_text = browser.find_element_by_id('cites').text
+eredmeny = browser.find_element_by_id('result')
 
-for i in varosok_text_list:
-    i = i.strip()
+varosok_osszes_list = str(varosok_osszes_text).split(', ')
+varosok_osszes = []
+for v in varosok_osszes_list:
+    varosok_osszes.append(v.strip('"'))
+
+for v in varosok_osszes:
     varos_input.clear()
-    varos_input.send_keys(i[1:-1])
+    varos_input.send_keys(v)
     ellenorzes_btn.click()
-    eredmeny = browser.find_element_by_id("result").text
-    if eredmeny != "Nem találtad el.":
+    if eredmeny.text == 'Eltaláltad.':
         break
 
-assert eredmeny == "Eltaláltad."
+assert eredmeny.text == 'Eltaláltad.'
 
 time.sleep(2)
 browser.quit()
