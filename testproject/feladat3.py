aa5db5d782b6467730f4f3ae083aeeebbe012026
krfprogit <krfprogit@gmail.com>
2021-08-23 12:40:32 +0200

    kész: elif

--- testproject/feladat3.py
+++ testproject/feladat3.py
@@ -8,5 +8,27 @@ URL = "https://black-moss-0a0440e03.azurestaticapps.net/ioa8.html"
 browser.get(URL)
 browser.maximize_window()
 
-# time.sleep(2)
-# browser.quit()
+# input
+operandus_1 = browser.find_element_by_id('num1').text
+operandus_2 = browser.find_element_by_id('num2').text
+operator = browser.find_element_by_id('op').text
+kalkulacio_btn = browser.find_element_by_id('submit')
+eredmeny = browser.find_element_by_id('result')
+
+operandus_1 = int(operandus_1)
+operandus_2 = int(operandus_2)
+if operator == '+':
+    szamolas = operandus_1 + operandus_2
+elif operator == '-':
+    szamolas = operandus_1 - operandus_2
+elif operator == '*':
+    szamolas = operandus_1 * operandus_2
+else:
+    szamolas = operandus_1 / operandus_2
+
+kalkulacio_btn.click()
+
+assert str(szamolas) == eredmeny.text
+
+time.sleep(2)
+browser.quit()
