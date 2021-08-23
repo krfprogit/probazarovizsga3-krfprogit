962773e35177f39a0234a4ab795a443f72e3356b
krfprogit <krfprogit@gmail.com>
2021-08-23 12:20:16 +0200

    kész

--- testproject/feladat2.py
+++ testproject/feladat2.py
@@ -8,5 +8,17 @@ URL = "https://black-moss-0a0440e03.azurestaticapps.net/tts4.html"
 browser.get(URL)
 browser.maximize_window()
 
-# time.sleep(2)
-# browser.quit()
+# input
+penzfeldobas_btn = browser.find_element_by_id('submit')
+utolso_eredmeny = browser.find_element_by_id('lastResult')
+penzfeldobas_fej_db = 0
+
+for i in range(100):
+    penzfeldobas_btn.click()
+    if utolso_eredmeny.text == "fej":
+        penzfeldobas_fej_db += 1
+
+assert penzfeldobas_fej_db >= 30
+
+time.sleep(1)
+browser.quit()
