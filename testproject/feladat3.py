d399d6a4a0c3be3a86100f5684c54bcc373ede0e
krfprogit <krfprogit@gmail.com>
2021-08-23 12:53:03 +0200

    kész: import operator

--- testproject/feladat3.py
+++ testproject/feladat3.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+import operator
 
 PATH = "C:\\Windows\\chromedriver.exe"
 browser = webdriver.Chrome(PATH)
@@ -9,24 +10,23 @@ browser.get(URL)
 browser.maximize_window()
 
 # input
-operandus_1 = browser.find_element_by_id('num1').text
-operandus_2 = browser.find_element_by_id('num2').text
-operator = browser.find_element_by_id('op').text
+operandus_1_input = browser.find_element_by_id('num1').text
+operandus_2_input = browser.find_element_by_id('num2').text
+operator_input = browser.find_element_by_id('op').text
 kalkulacio_btn = browser.find_element_by_id('submit')
 eredmeny = browser.find_element_by_id('result')
 
-operandus_1 = int(operandus_1)
-operandus_2 = int(operandus_2)
-if operator == '+':
-    szamolas = operandus_1 + operandus_2
-elif operator == '-':
-    szamolas = operandus_1 - operandus_2
-elif operator == '*':
-    szamolas = operandus_1 * operandus_2
-else:
-    szamolas = operandus_1 / operandus_2
-
+operandus_1_input = int(operandus_1_input)
+operandus_2_input = int(operandus_2_input)
+ops = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv
+}
 kalkulacio_btn.click()
+
+szamolas = ops[operator_input](operandus_1_input, operandus_2_input)
 
 assert str(szamolas) == eredmeny.text
 
